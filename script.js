463b556d3e2d63ae562a96a2c76c12ddf8d451fc
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-16 18:34:54 +0800

    adjust grid

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const buttons = document.querySelector('.buttons');
 const clearButton = document.querySelector('.clear-button');
 const gridButton = document.querySelector('.grid-button');
 const container = document.querySelector('.container');
-const sizes = 30;
-let grid = 16;
+const sizes = 15;
+let grid = 30;
 var tempColor = 'red';
 
 createDiv(grid, grid);
